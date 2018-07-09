43f91cc8600c7cd00b94071f1928c08a2093ce03
radiium <remip.dev@gmail.com>
2018-07-09 16:39:23 +0200

    Refactor download api

--- src-electron/scripts/download.ts
+++ src-electron/scripts/download.ts
@@ -6,114 +6,135 @@ import ffmpeg = require('fluent-ffmpeg');
 import ffmpegStatic = require('ffmpeg-static');
 import fs = require('fs-extra');
 import path = require('path');
+import _ = require('lodash');
 
 import { handleError } from './error';
 
-console.log(ffmpegStatic.path);
-console.log('temp', app.getPath('temp'));
+console.log('Set ffmpeg path', ffmpegStatic.path);
+ffmpeg.setFfmpegPath(ffmpegStatic.path);
 
+export const streamList = {};
 
 // ----------------------------------------------------------------------------
 // Download management
 
 export function initDownloadEvent() {
-    ipcMain.on('onDownload', (event, arg) => {
-        console.log('==> onDownload =======================');
+    ipcMain.on('download', (event, arg) => {
+        console.log('=================');
+        console.log('==> onDownload');
 
-        const index = arg.index;
-        const videoId = arg.videoId;
-        const filePath = arg.filePath;
-        const fileName = arg.fileName;
-
-        console.log('index', index);
-        console.log('videoId', videoId);
-        console.log('filePath', filePath);
-        console.log('fileName', fileName);
-
-        if (!checkDownloadParam(index, videoId, filePath, fileName, event)) {
+        if (!checkDownloadParam(arg, event)) {
             return;
         }
 
-        download(event, index, videoId, fileName);
+        createStream(event, arg.video, arg.savePath);
+    });
+
+    ipcMain.on('onCancelDownload', (event, arg) => {
+        console.log('=================');
+        console.log('==> onCancelDownload', streamList);
+
+        for (const key in streamList) {
+            if (streamList.hasOwnProperty(key)) {
+
+                // streamList[key].convertStream.ffmpegProc.kill();
+                streamList[key].readStream._destroy((err) => {
+                    console.log('CLOSE readStream', err);
+                });
+            }
+        }
     });
 }
 
-
-
-function download(event, index, videoId, fileName) {
-
-    console.log('==> download');
+function createStream(event, video, savePath) {
 
     let startDL;
-    fileName = sanitize(fileName, '') + '.tmp_download';
-    const TMP_OUT = path.resolve(app.getPath('desktop'), 'dll', fileName);
-    // app.getPath('temp')
+    const fileName = sanitize(video.title, '');
+    const OUTPUT = path.resolve(savePath, fileName + '.mp3');
 
-    console.log('fileName', fileName);
-    console.log('TMP_OUT', TMP_OUT);
 
-    const ytFile = ytdl(videoId, {
-        quality: 'highestaudio',
-        // filter: 'audioonly',
-    });
-
-    ytFile.pipe(fs.createWriteStream(TMP_OUT));
-
-    ytFile.once('response', () => {
-        console.log('start');
+    const readStream = ytdl(video.id, { quality: 'highestaudio' })
+    .once('response', (response) => {
+        console.log(`[DOWNLOAD] ${video.id} => START`);
         startDL = Date.now();
-        event.sender.send('downloadProgress', { type: 'download', status: 'start', index: index });
+        event.sender.send('onDownloadStart', { id: video.id });
+    })
+    .on('progress', (chunkLength, downloaded, total) => {
+        const data = computeProgress(downloaded, total, startDL, video.id);
+        // console.log(`[DOWNLOAD] ${video.id} ytdl => PROGRESS`, data.progress.percent);
+        event.sender.send('onDownloadProgress', data);
+    })
+    .on('error', (err) => {
+        console.log(`[DOWNLOAD] ${video.id} => ERROR => `, err);
+        event.sender.send('onDownloadError', { id: video.id, error: err });
+        cancel(event, video, 'DOWNLOAD_ERROR');
+    })
+    .on('end', () => {
+        console.log(`[DOWNLOAD] ${video.id} => END`);
+    })
+    .on('close', () => {
+        console.log(`[DOWNLOAD] ${video.id} => CLOSE`);
+    })
+    .on('finish', () => {
+        console.log(`[DOWNLOAD] ${video.id} => FINISH`);
     });
 
-    ytFile.on('progress', (chunkLength, downloaded, total) => {
-        // console.log('DOWNLOAD => progress');
-        const floatDownloaded = downloaded / total;
-        const downloadedMinutes = (Date.now() - startDL) / 1000 / 60;
+    const ffmpegCmd = ffmpeg(readStream)
+        .audioBitrate(192)
+        .audioCodec('libmp3lame')
+        .toFormat('mp3')
+        .save(OUTPUT)
+        .on('error', (err) => {
+            console.log(`[CONVERT] ${video.id} => ERROR`, err);
+            event.sender.send('onDownloadError', { id: video.id, error: err });
+            cancel(event, video, 'CONVERT_ERROR');
+        })
+        .on('end', () => {
+            console.log(`[CONVERT] ${video.id} END`);
+            event.sender.send('onDownloadEnd', { id: video.id });
+        });
 
-        const data = {
-            type: 'download',
-            status: 'progress',
-            index: index,
-            progress: {
-                percent: (floatDownloaded * 100).toFixed(2),
-                downloaded: (downloaded / 1024 / 1024).toFixed(2),
-                total: (total / 1024 / 1024).toFixed(2),
-                mn: downloadedMinutes.toFixed(2),
-                mnRest: (downloadedMinutes / floatDownloaded - downloadedMinutes).toFixed(2)
-            }
-        };
-
-        // console.log('progress', data);
-        event.sender.send('downloadProgress', data);
+    ipcMain.on('cancelDownload.' + video.id, (evt, arg) => {
+        cancel(evt, video, 'CANCELED');
     });
 
-    ytFile.on('end', () => {
-        console.log('ended');
-        event.sender.send('downloadProgress', { type: 'download', status: 'ended', index: index });
-    });
+    const cancel = (ev, vid, from) => {
+        console.log(`[${from}] ${video.id} => `, vid.id);
+        readStream.destroy(new Error(``));
+        ffmpegCmd.kill();
+        if (fs.existsSync(OUTPUT)) {
+            fs.unlinkSync(OUTPUT);
+        }
+        ev.sender.send('onDownloadCancel', { id: video.id });
+    };
 }
 
+function computeProgress(downloaded, total, startDL, id) {
+    const floatDownloaded = downloaded / total;
+    const downloadedMinutes = (Date.now() - startDL) / 1000 / 60;
+    return {
+        id: id,
+        progress: {
+            percent: (floatDownloaded * 100).toFixed(2),
+            downloaded: (downloaded / 1024 / 1024).toFixed(2),
+            total: (total / 1024 / 1024).toFixed(2),
+            mn: downloadedMinutes.toFixed(2),
+            mnRest: (downloadedMinutes / floatDownloaded - downloadedMinutes).toFixed(2)
+        }
+    };
+}
 
-
-function checkDownloadParam(event, index, videoId, filePath, fileName) {
-
-    console.log('==> check params');
-
-    if (!videoId) {
-        handleError('Error, wrong videoId: ' + videoId, event.sender);
-    }
-    if (!filePath) {
-        handleError('Error, wrong filePath: ' + filePath, event.sender);
-    }
-    if (!fileName) {
-        handleError('Error, wrong fileName: ' + fileName, event.sender);
-    }
-    if (!videoId || !fileName || !filePath) {
+function checkDownloadParam(arg, event) {
+    console.log('[DOWNLOAD] ==> check params', arg);
+    if (!arg.savePath) {
+        handleError('[DOWNLOAD] Error, missing savePath', event.sender);
         return false;
     }
-
-    console.log('==> check params done!');
-
+    if (!arg.video) {
+        handleError('[DOWNLOAD] Error, missing video', event.sender);
+        return false;
+    }
+    console.log('[DOWNLOAD] ==> check params done!');
     return true;
 }
 
